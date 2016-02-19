f39b1c4ddff5d0d6c6caf1ae12d4ccc0070dea02
Cameron Stoner <wolfbinary@gmail.com>
2016-02-18 21:04:05 -0600

    start of creating reusuable html components and helpers.

---
+++ YourGrowth/Helpers/AdminPanel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YourGrowth.Web.Helpers
+{
+    public class AdminHelper
+    {
+        public static string AdminPanel(string id, string linkTitle, string panelId, string innerHtml)
+        {
+            return string.Format(@"
+                <a href="#">{0}</a>
+        < div id = \"{1}" class="panel">
+            <div class="panel-body">
+
+            </div>
+        </div>
+");
+        }
+    }
+}

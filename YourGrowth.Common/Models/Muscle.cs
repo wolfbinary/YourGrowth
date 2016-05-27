25828c9b75b684c9b3f46bcdf752a079f2b51c97
Cameron Stoner <wolfbinary@gmail.com>
2016-05-27 16:42:41 -0500

    Muscles Model added

---
+++ YourGrowth.Common/Models/Muscle.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YourGrowth.Common.Models
+{
+    public class Muscle
+    {
+        [Required]
+        public int MuscleId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
